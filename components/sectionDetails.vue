a4a855b93953e83adb80d62c4a061c3dd47a3d47
Maxime KERLIDOU <mkerlidou@goweb.fr>
2023-02-20 17:01:30 +0100

    refacto

--- components/sectionDetails.vue
+++ components/sectionDetails.vue
@@ -1,5 +1,18 @@
 <template>
   <h1 class="text-white">{{ category.category }}</h1>
+  <section class="" :movie="movie">
+    <div class="flex flex-nowrap overflow-y-scroll">
+      <div v-for="image in category.images" class="flex flex-shrink-0">
+        <img
+          :src="image"
+          alt=""
+          height="200"
+          width="300"
+          class="object-cover"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
